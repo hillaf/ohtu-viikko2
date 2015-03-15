acb4117c1968e01761e7ff49c76a3c41e827dbd8
scontro <hilla.fred@helsinki.fi>
2015-03-15 17:26:02 +0200

    tests

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -64,6 +64,38 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+    
+    @Test
+    public void eiVoiLisataYliTilavuuden() {
+        varasto.lisaaVarastoon(8);
+
+        varasto.lisaaVarastoon(3);
+
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiVoiLisataNegatiivista() {
+        varasto.lisaaVarastoon(-1);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiVoiOttaaNegatiivista() {
+        varasto.otaVarastosta(-1);
+
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiVoiOttaaYliSaldon() {
+        varasto.lisaaVarastoon(3);
+
+        varasto.otaVarastosta(4);
+
+        assertEquals(3, varasto.getSaldo(), vertailuTarkkuus);
+    }
 
     @Test
     public void konstr() {
